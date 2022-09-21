90dc846d97d76b96be8fc45544a64985d0bf942c
influ <26268158+ahmed316dev@users.noreply.github.com>
2022-09-21 14:21:10 +0300

    finished

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,46 @@
+<template>
+  <div class="app">
+    <Sidebar />
+    <router-view />
+  </div>
+</template>
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from './components/Sidebar.vue'
 </script>
 
-<template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
+<style lang="scss">
+:root {
+  --primary: #4ade80;
+  --grey: #64748b;
+  --dark: #1e293b;
+  --dark-alt: #334155;
+  --light: #f1f5f9;
+  --sidebar-width: 300px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Fira sans', sans-serif;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+body {
+  background: var(--light);
+}
+button {
+  cursor: pointer;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+}
+.app {
+  display: flex;
+  main {
+    flex: 1 1 0;
+    padding: 2rem;
+    // @media (max-width: 768px) {
+    //   padding-left: 6rem;
+    // }
+  }
 }
 </style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <main class="about-page">
+    <h1>about</h1>
+    <p>this is the about page</p>
+  </main>
+</template>
